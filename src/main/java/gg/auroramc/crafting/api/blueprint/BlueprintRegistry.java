15a84e5abf23333a143df4c564e6bb34cb030543
Erik Szabó <erik1szabo1@gmail.com>
2025-02-07 23:14:29 +0100

    Add result based lookup to BlueprintRegistry

--- src/main/java/gg/auroramc/crafting/api/blueprint/BlueprintRegistry.java
+++ src/main/java/gg/auroramc/crafting/api/blueprint/BlueprintRegistry.java
@@ -1,21 +1,26 @@
 package gg.auroramc.crafting.api.blueprint;
 
+import gg.auroramc.aurora.api.item.TypeId;
 import gg.auroramc.crafting.api.workbench.Workbench;
 import gg.auroramc.crafting.api.workbench.WorkbenchRegistry;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class BlueprintRegistry {
     private final Map<String, Blueprint> blueprints;
+    private final Map<TypeId, List<Blueprint>> resultLookup;
 
-    private BlueprintRegistry(Map<String, Blueprint> blueprints) {
+    private BlueprintRegistry(Map<String, Blueprint> blueprints, Map<TypeId, List<Blueprint>> resultLookup) {
         this.blueprints = Map.copyOf(blueprints);
+        this.resultLookup = Map.copyOf(resultLookup);
     }
 
     public Blueprint getBlueprint(String id) {
         return blueprints.get(id);
+    }
+
+    public List<Blueprint> getBlueprintsFor(TypeId item) {
+        return resultLookup.getOrDefault(item, Collections.emptyList());
     }
 
     public Collection<Blueprint> getBlueprints() {
@@ -24,21 +29,23 @@ public class BlueprintRegistry {
 
     public static BlueprintRegistry createFrom(WorkbenchRegistry workbenchRegistry) {
         Map<String, Blueprint> collectedBlueprints = new HashMap<>();
+        Map<TypeId, List<Blueprint>> resultLookupBlueprints = new HashMap<>();
 
-        addWorkbenchBlueprints(collectedBlueprints, workbenchRegistry.getCraftingTable());
-        addWorkbenchBlueprints(collectedBlueprints, workbenchRegistry.getSmithingTable());
-        addWorkbenchBlueprints(collectedBlueprints, workbenchRegistry.getFurnace());
+        addWorkbenchBlueprints(collectedBlueprints, resultLookupBlueprints, workbenchRegistry.getCraftingTable());
+        addWorkbenchBlueprints(collectedBlueprints, resultLookupBlueprints, workbenchRegistry.getSmithingTable());
+        addWorkbenchBlueprints(collectedBlueprints, resultLookupBlueprints, workbenchRegistry.getFurnace());
 
         for (Workbench workbench : workbenchRegistry.getCustomWorkbenches()) {
-            addWorkbenchBlueprints(collectedBlueprints, workbench);
+            addWorkbenchBlueprints(collectedBlueprints, resultLookupBlueprints, workbench);
         }
 
-        return new BlueprintRegistry(collectedBlueprints);
+        return new BlueprintRegistry(collectedBlueprints, resultLookupBlueprints);
     }
 
-    private static void addWorkbenchBlueprints(Map<String, Blueprint> blueprints, Workbench workbench) {
+    private static void addWorkbenchBlueprints(Map<String, Blueprint> blueprints, Map<TypeId, List<Blueprint>> resultLookupBlueprints, Workbench workbench) {
         for (var blueprint : workbench.getBlueprints()) {
             blueprints.put(blueprint.getId(), blueprint);
+            resultLookupBlueprints.computeIfAbsent(blueprint.getResult().id(), k -> new ArrayList<>()).add(blueprint);
         }
     }
 }
